701d1525c9ae5997b4c759db4459686df6c3a47b
tomuzca <tomuzcategui@gmail.com>
2025-10-15 09:44:51 -0300

    agregando las pag

--- wp-content/themes/cimptheme/header.php
+++ wp-content/themes/cimptheme/header.php
@@ -7,18 +7,18 @@
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
-          <a href="#"><strong>CMIP</strong></a>
+          <a href="<?php echo site_url ('/home') ?>"><strong>CMIP</strong></a>
         </h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
-              <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="<?php echo site_url ('/about-us') ?>">About Us</a></li>
+              <li><a href="<?php echo site_url ('/services') ?>">Services</a></li>
+              <li><a href="<?php echo site_url ('/work-with-us') ?>">Work with us</a></li>
+              <li><a href="<?php echo site_url ('/subcontract') ?>">Subcontractors & Partners</a></li>
+              <li><a href="<?php echo site_url ('/contact-us') ?>">Contact Us</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
