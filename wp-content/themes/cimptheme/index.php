d7a6db4928db89401ba3f0fd7132d2a0a0d3f77d
tomuzca <tomuzcategui@gmail.com>
2025-10-17 10:37:06 -0300

    index

--- wp-content/themes/cimptheme/index.php
+++ wp-content/themes/cimptheme/index.php
@@ -1,88 +1,38 @@
-<?php get_header(); ?>
+<?php
 
-  <div class="page-banner">
-      <div class="page-banner__bg-image">
-        <video autoplay muted loop playsinline style="position: absolute; width: 100%; height: 100%; object-fit: cover;">
-          <source src="<?php echo get_theme_file_uri('/images/blueprints.mp4') ?>" type="video/mp4">
-        </video>
+get_header(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/wip.png') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">Welcome to Our Community</h1>
+      <div class="page-banner__intro">
+        <p>Keep up with our latest projects</p>
       </div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">CMIP</h1>
-        <h2 class="headline headline--medium">Engineering & Construction</h2></br>
-        <h3 class="headline headline--small">'A' Licensed General Engineering Contractor Company </br>'B' Licensed General Builder Contractor Company </h3>
-        <h3 class="headline headline--small">California CSLB# 1115725</h3>
+    </div>  
+  </div>
+
+
+<div class="container container--narrow page-section">
+  <?php
+  while(have_posts()) {
+    the_post(); ?>
+    <div class="post-item">
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      
+      <div class="metabox">
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
+
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+      </div>
+
     </div>
-
-    <div class="full-width-split group">
-      <div class="full-width-split__one">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">Licenses & Certifications</h2>
-
-          <div class="event-summary">
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">CMIP LLC is licensed by the California 's Contractors State License Board as:</a></h5>
-              <p><strong>Class A General Engineering Contractor License</strong> and <strong>Class B General Builder Contractor License.</strong></p>
-            </div>
-          </div>
-          <div class="event-summary">           
-            <div class="event-summary__content">
-              <p>Also, the company is a Certified Small Business Entity by the SBA, the DGS, the Los Angeles County and the Port of Long Beach.</p>
-            </div>
-          </div>          
-        </div>
-      </div>
-      <div class="full-width-split__two" > <!--style="background-color: #ffc765ff;"-->
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">Our Expertise</h2>
-
-          <div class="event-summary">
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny">With over 10 years of experience, our team excels in all aspects of construction, from planning and design to execution and management. Led by a licensed General Contractor with A and B licenses, a Civil Engineer, and a CQM-certified professional, we bring technical expertise and meticulous attention to detail to every project.<br> Our extensive knowledge and hands-on experience ensure that we consistently deliver exceptional results for public works and federal projects. </h5>              
-            </div>
-          </div>         
-        </div>
-      </div>
-    </div>
-
-    <div class="hero-slider">
-      <div data-glide-el="track" class="glide__track">
-        <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/logo.png') ?>);">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">About CMIP</h2>
-                <p class="t-center">We are a California-based General Contracting Company specializing in the Commercial Construction Industry.</p> 
-                <p class="t-center">Our expertise lies in managing and executing public, state and federal projects with professionalism and commitment to excellence. </p><p class="t-center"> CMIP has substantially delivered high-quality results for our clients.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/planos.png') ?>);">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Our Expertise</h2>
-                <p class="t-center">With over 10 years of experience, our team excels in all aspects of construction, from planning and design to execution and management. </p>
-                <p class="t-center">Led by a licensed General Contractor with A and B licenses, a Civil Engineer, and a CQM-certified professional, we bring technical expertise and meticulous attention to detail to every project.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/licenses.png') ?>);">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Licenses & Certifications </h2>
-                <p class="t-center">CMIP LLC is licensed by the California 's Contractors State License Board as:  Class A General Engineering Contractor License and Class B General Builder Contractor License.</p>
-                  <p class="t-center"> Also, the company is a Certified Small Business Entity by the SBA, the DGS, the Los Angeles County and the Port of Long Beach.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
-      </div>
-    </div>
-
-  <?php get_footer();
+  <?php }
+?>
+</div>
+<?php get_footer();
 
 ?>
