502095442ad0913ae2b35b7c662faca0e702084f
tomuzca <tomuzcategui@gmail.com>
2025-10-17 11:23:41 -0300

    mod

--- wp-content/themes/cimptheme/index.php
+++ wp-content/themes/cimptheme/index.php
@@ -3,14 +3,18 @@
 get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/wip.png') ?>);"></div>
+  <div class="page-banner__bg-image">
+        <video autoplay muted loop playsinline style="position: absolute; width: 100%; height: 100%; object-fit: cover;">
+          <source src="<?php echo get_theme_file_uri('/images/blueprints.mp4') ?>" type="video/mp4">
+        </video>
+  </div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title">Welcome to Our Community</h1>
       <div class="page-banner__intro">
         <p>Keep up with our latest projects</p>
       </div>
-    </div>  
-  </div>
+      </div>  
+</div>
 
 
 <div class="container container--narrow page-section">
--- wp-content/themes/cimptheme/page.php
+++ wp-content/themes/cimptheme/page.php
@@ -6,7 +6,11 @@
     the_post(); ?>
     
     <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/wip.png') ?>);"></div>
+      <div class="page-banner__bg-image">
+        <video autoplay muted loop playsinline style="position: absolute; width: 100%; height: 100%; object-fit: cover;">
+          <source src="<?php echo get_theme_file_uri('/images/blueprints.mp4') ?>" type="video/mp4">
+        </video>
+      </div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
