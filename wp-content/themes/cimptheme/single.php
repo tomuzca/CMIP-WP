1501cc89741deff216276fbb9fe1a0d620cd8cf6
tomuzca <tomuzcategui@gmail.com>
2025-10-17 11:27:09 -0300

    single

--- wp-content/themes/cimptheme/single.php
+++ wp-content/themes/cimptheme/single.php
@@ -4,8 +4,30 @@
 
   while(have_posts()) {
     the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image">
+        <video autoplay muted loop playsinline style="position: absolute; width: 100%; height: 100%; object-fit: cover;">
+          <source src="<?php echo get_theme_file_uri('/images/blueprints.mp4') ?>" type="video/mp4">
+        </video>
+      </div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DONT FORGET TO REPLACE ME LATER</p>
+        </div>
+      </div>  
+    </div>
+
+    <div class="container container--narrow page-section">
+          <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      </div>
+
+      <div class="generic-content"><?php the_content(); ?></div>
+
+    </div>
+    
+
     
   <?php }
 
