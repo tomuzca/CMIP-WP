cdc4627520fd81ebe55e2020161dd5442d7c8956
tomuzca <tomuzcategui@gmail.com>
2025-10-21 08:04:06 -0300

    header sin botones

--- wp-content/themes/cimptheme/header.php
+++ wp-content/themes/cimptheme/header.php
@@ -29,11 +29,11 @@
             </ul>
             -->
           </nav>
-          <div class="site-header__util">
+         <!--  <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
             <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
             <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-          </div>
+          </div>-->
         </div>
       </div>
     </header>
